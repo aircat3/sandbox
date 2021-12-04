1e27ea946846fb462590da08d18f2d92d5b1f001
aircat3 <aircat333@gmail.com>
2021-12-04 22:47:46 +0900

    GUIレイアウトの基本イメージを作成

--- FF14Marketer/main.py
+++ FF14Marketer/main.py
@@ -1,16 +1,23 @@
 import PySimpleGUI as sg
 
+# Windowテーマの設定
+#sg.theme_previewer()
 sg.theme('dark grey 9')
 
 # Windowレイアウトの設定
 layout = [
-    [sg.Text('ここは1行目')],
-    [sg.Text('ここは2行目：文字を入力してください'), sg.InputText()],
+    [sg.Text('マーケット記録画面')],
+    [sg.Text('アイテム名', size=(10, 1)), sg.InputText()],
+    [sg.Text('HQ品', size=(10, 1)), sg.InputText()],
+    [sg.Text('単価', size=(10, 1)), sg.InputText()],
+    [sg.Text('個数', size=(10, 1)), sg.InputText()],
+    [sg.Text('購入者名', size=(10, 1)), sg.InputText()],
+    [sg.Text('取引時刻', size=(10, 1)), sg.InputText()],
     [sg.Button('OK'), sg.Button('キャンセル')]
 ]
 
 # Windowの生成
-window = sg.Window('サンプルプログラム', layout)
+window = sg.Window('サンプルプログラム', layout, element_justification='center')
 
 # イベント処理
 while True:
