7600d5b1c3e72f3aa596ceb3e0b99dd67eb2218f
aircat3 <aircat333@gmail.com>
2021-12-28 08:38:37 +0900

    DAOの変数名が分かりづらいので修正

--- FF14Marketer/DAO.py
+++ FF14Marketer/DAO.py
@@ -1,21 +1,21 @@
 import sqlite3
  
 # 接続。なければDBを作成する。
-conn = sqlite3.connect('databases/example.db')
+db_con = sqlite3.connect('databases/example.db')
  
 # カーソルを取得
-c = conn.cursor()
- 
+db_cur = db_con.cursor()
+
 # テーブルを作成
-c.execute('CREATE TABLE articles  (id int, title varchar(1024), body text, created datetime)')
+db_cur.execute('CREATE TABLE articles  (id int, title varchar(1024), body text, created datetime)')
  
 # Insert実行
-c.execute("INSERT INTO articles VALUES (1,'今朝のおかず','魚を食べました','2020-02-01 00:00:00')")
-c.execute("INSERT INTO articles VALUES (2,'今日のお昼ごはん','カレーを食べました','2020-02-02 00:00:00')")
-c.execute("INSERT INTO articles VALUES (3,'今夜の夕食','夕食はハンバーグでした','2020-02-03 00:00:00')")
+db_cur.execute("INSERT INTO articles VALUES (1,'今朝のおかず','魚を食べました','2020-02-01 00:00:00')")
+db_cur.execute("INSERT INTO articles VALUES (2,'今日のお昼ごはん','カレーを食べました','2020-02-02 00:00:00')")
+db_cur.execute("INSERT INTO articles VALUES (3,'今夜の夕食','夕食はハンバーグでした','2020-02-03 00:00:00')")
  
 # コミット
-conn.commit()
+db_con.commit()
  
 # コネクションをクローズ
-conn.close()
+db_con.close()
