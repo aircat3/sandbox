dad8de3dcf7e610e8ffcc726b2fa04056f489bab
aircat3 <aircat333@gmail.com>
2022-02-15 14:06:13 +0900

    複数回対戦を試行できるように修正

--- Toybox/Janken/main.py
+++ Toybox/Janken/main.py
@@ -6,6 +6,9 @@ if __name__ == "__main__":
     non_player_1 = 0
     non_player_2 = 0
 
+    # NPC2の勝利数カウント
+    win_count = 0
+
     # グーチョキパーの定義
     GUTYPA = {
         0 : "グー",
@@ -23,17 +26,31 @@ if __name__ == "__main__":
         [WIN,  LOSE, DRAW]  # P1 ぱー
     ]
 
-    # キャラクターの手を決める
-    non_player_1 = random.randint(0, 2)
-    non_player_2 = random.randint(0, 2)
+    # 対戦試行回数
+    tryals = 10
+    for battle_count in range(tryals):
 
-    # 勝敗決定
-    print("[Player1:" + GUTYPA[non_player_1] + "]")
-    print("[Player2:" + GUTYPA[non_player_2] + "]")
-    result = JUDGE[non_player_1][non_player_2]
+        # キャラクターの手を決める
+        non_player_1 = random.randint(0, 2)
+        non_player_2 = random.randint(0, 2)
+
+        # 勝敗決定
+        #print("[Player1:" + GUTYPA[non_player_1] + "]")
+        #print("[Player2:" + GUTYPA[non_player_2] + "]")
+        result = JUDGE[non_player_1][non_player_2]
+
+        # Player2の勝利数カウント
+        if result == LOSE : win_count += 1
+
+    print("Player2の勝利数:" + str(win_count))
+
+    """
     if result == WIN:
         print("Player1の勝利です。")
     elif result == LOSE:
         print("Player2の勝利です。")
     elif result == DRAW:
         print("引き分けです。")
+    """
+    
+    
