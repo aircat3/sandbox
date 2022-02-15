73437bc6b48468c56fde58b518bb133672c8244e
aircat3 <aircat333@gmail.com>
2022-02-15 17:51:47 +0900

    大修正：勝利数のカウント方法の変更とjudgeメソッドで結果を返していなかったので修正

--- Toybox/Janken/main.py
+++ Toybox/Janken/main.py
@@ -1,16 +1,15 @@
 from cgitb import reset
 import random
+from unittest import result
 
 class PlayCharctor:
     def __init__(self, name):
         self.name = name
         self.hand = None
-        self.cnt_win = 0
-        self.cnt_lose =0
-    def countUp_win(self):
-        self.cnt_win += 1
-    def countUp_lose(self):
-        self.cnt_lose += 1
+        # kill/death
+        self.KD = {'win':0, 'lose':0, 'draw':0}
+    def countUp_KD(self, result):
+        self.KD[result] += 1
 
 # ジャンケンの判定結果を返す。判定基準はplayer1に対して
 def judge(player1: PlayCharctor, player2: PlayCharctor):
@@ -18,6 +17,7 @@ def judge(player1: PlayCharctor, player2: PlayCharctor):
     if(0 < result): result = 'win'
     elif(0 > result): result = 'lose'
     else: result = 'draw'
+    return result
 
 if __name__ == "__main__":
 
@@ -35,41 +35,16 @@ if __name__ == "__main__":
         0 : "パー"
     }
 
-    # 勝敗定義
-    WIN = 1
-    DRAW = 0
-    LOSE = -1
-    JUDGE = [
-        [DRAW, WIN,  LOSE], # P1 ぐー
-        [LOSE, DRAW, WIN],  # P1 ちょき
-        [WIN,  LOSE, DRAW]  # P1 ぱー
-    ]
-
     # 対戦試行回数
-    tryals = 10
+    tryals = 3
     for battle_count in range(tryals):
 
         # キャラクターの手を決める
-        non_player_1 = random.randint(0, 2)
-        non_player_2 = random.randint(0, 2)
+        players['npc1'].hand = random.randint(0, 2)
+        players['npc2'].hand = random.randint(0, 2)
 
         # 勝敗決定
-        #print("[Player1:" + GUTYPA[non_player_1] + "]")
-        #print("[Player2:" + GUTYPA[non_player_2] + "]")
-        result = JUDGE[non_player_1][non_player_2]
+        result = judge(players['npc1'], players['npc2'])
 
-        # Player2の勝利数カウント
-        if result == LOSE : win_count += 1
-
-    print("Player2の勝利数:" + str(win_count))
-
-    """
-    if result == WIN:
-        print("Player1の勝利です。")
-    elif result == LOSE:
-        print("Player2の勝利です。")
-    elif result == DRAW:
-        print("引き分けです。")
-    """
-    
-    
+        # 勝敗カウント
+        players['npc1'].countUp_KD(result)
