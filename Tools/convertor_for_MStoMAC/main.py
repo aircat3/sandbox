fb7f321705c139c6c9da606009344c1c2abe7ef0
aircat3 <aircat333@gmail.com>
2022-02-23 08:25:18 +0900

    カレントディレクトリの取得とファイルオープン処理を記述

--- Tools/convertor_for_MStoMAC/main.py
+++ Tools/convertor_for_MStoMAC/main.py
@@ -1,9 +1,17 @@
 # MS辞書データ(csv.txt)をMAC辞書データ(.plist)に変換する
+import os
 
 def main():
 
+    # カレントディレクトリ取得
+    path = os.getcwd()
+    print("############")
+    print("[Sys]" + path)
+    print("############")
+
     # 変換元ファイルを開く
-    file = open('myfile.txt', 'r')
+    file_name = '/sangokushi.txt'
+    file = open(path + file_name, 'r', encoding="shift-jis")
 
     for data in file:
         print(data)
