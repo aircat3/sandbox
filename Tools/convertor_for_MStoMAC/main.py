2acef83c25a88bc4e36fcab7d8c35559555558a9
aircat3 <aircat333@gmail.com>
2022-02-23 08:36:22 +0900

    plistの構造文を書き出す処理を記述

--- Tools/convertor_for_MStoMAC/main.py
+++ Tools/convertor_for_MStoMAC/main.py
@@ -13,10 +13,35 @@ def main():
     file_name = '/sangokushi.txt'
     read_file = open(path + file_name, 'r', encoding="shift-jis")
 
+    # 変換先ファイルを作成
+    file_name = '/sangokushi.plist'
+    write_file = open(path + file_name, 'w', encoding='UTF-8')
+    
+    # .plistの先頭文を記述
+    words = '<?xml version="1.0" encoding="UTF-8"?>'
+    write_file.write(words + '\n')
+    words = '<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">'
+    write_file.write(words + '\n')
+    words = '<plist version="1.0">'
+    write_file.write(words + '\n')
+    words = '<array>'
+    write_file.write(words + '\n')
+    words = '<plist>'
+    write_file.write(words + '\n')
+
     # 書き出していく
     for data in read_file:
         print(data)
+
+    # .plistの終端文を記述
+    words = '</array>'
+    write_file.write(words + '\n')
+    words = '</plist>'
+    write_file.write(words + '\n')
+
+    # ファイルクローズ
     read_file.close()
+    write_file.close()
 
 if __name__ == "__main__":
     main()
