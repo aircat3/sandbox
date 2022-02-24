819da846150316cd71ac7507bf6c811de68e307a
aircat3 <aircat333@gmail.com>
2022-02-25 08:37:11 +0900

    ui.pyの中身を他でも扱いやすいようにクラス化

--- Toybox/Sangoku/ui.py
+++ Toybox/Sangoku/ui.py
@@ -2,46 +2,56 @@ import PySimpleGUI as sg
 import tkinter as tk
 from PIL import Image, ImageTk
 
+
+class SimpleGUI():
+    def __init__(self):
+        title_name = 'Simulator'        # アプリタイトル
+        window_theme = 'SystemDefault'  # Windowテーマの設定
+        font_type = ('Arial',14)        # フォント指定
+
+        #sg.theme_previewer()
+        #sg.theme('dark grey 9')
+        sg.theme(window_theme)
+        
+        # windowsレイアウト設定
+        col1 = [
+            [sg.Text('戦闘状況')],
+            [sg.Multiline(
+                default_text="hello\nworld\n!!\n", 
+                font=font_type,
+                size=(40,12),
+                disabled=True,
+                key='-LOG-')
+            ]
+        ]
+        col2 = [[sg.Frame('部隊状況',[
+            [sg.Text('部隊１')],
+            [sg.Text('部隊２', size=(50,15))],
+        ])]]
+        col3 = [
+            [sg.Text('コマンド')],
+            [sg.InputText(
+                '', font=font_type, size=(100, 30),
+                enable_events=True, key='-INPUT-',
+            )]
+        ]
+        layout = [
+            [sg.Column(col1), sg.Column(col2)],
+            [sg.Column(col3)],
+            # inputでEnterキーを押された時用のダミーボタン
+            [sg.Button('Submit', visible=False, bind_return_key=True)]
+        ]
+
+        self.window = sg.Window(title_name, layout, size=(500, 300), margins=(0,0))
+    
+    def getWindow(self):
+        return self.window
+
 def initGUI():
 
-    # Windowテーマの設定
-    #sg.theme_previewer()
-    #sg.theme('dark grey 9')
-    sg.theme('SystemDefault')
+    simpleGUI = SimpleGUI()
+    window = simpleGUI.getWindow()
     
-    # Windowレイアウトの設定
-    col1 = [
-        [sg.Text('戦闘状況')],
-        [sg.Multiline(
-            default_text="hello\nworld\n!!\n", 
-            font=('Arial',14),
-            size=(40,12),
-            disabled=True,
-            key='-LOG-')
-        ]
-    ]
-    col2 = [[sg.Frame('部隊状況',[
-        [sg.Text('部隊１')],
-        [sg.Text('部隊２', size=(50,15))],
-    ])]]
-    col3 = [
-        [sg.Text('コマンド')],
-        [sg.InputText(
-            '', font=('Arial',14), size=(100, 30),
-            enable_events=True, key='-INPUT-',
-        )]
-    ]
-    layout = [
-        [sg.Column(col1), sg.Column(col2)],
-        [sg.Column(col3)],
-        # inputでEnterキーを押された時用のダミーボタン
-        [sg.Button('Submit', visible=False, bind_return_key=True)]
-    ]
-
-    # Windowの生成
-    window = sg.Window('サンプルプログラム', layout, size=(500, 300), margins=(0,0))
-    
-    # イベント処理
     while True:
         event, values = window.read()
         if event == sg.WIN_CLOSED or event == 'キャンセル':
@@ -56,10 +66,6 @@ def initGUI():
             # ログにテキストを追加し、入力エリアをクリア
             window['-LOG-'].print(input_text)
             window['-INPUT-'].update('')
-
-        elif event == 'OK':
-            print('ダウンロードURL', values[0])
-            print('ファイル保存場所', values[1])
 
     window.close()
 
