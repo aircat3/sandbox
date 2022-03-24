37a6d565ccdb49da61da3080e0c8d75c3bb25afd
aircat3 <aircat333@gmail.com>
2022-03-24 17:42:05 +0900

    windowの管理をEventManagerに行わせることにした

--- Toybox/Sangoku/EventManager.py
+++ Toybox/Sangoku/EventManager.py
@@ -1,13 +1,27 @@
+# システム系
+from asyncio import futures
 from collections import deque
-from concurrent.futures import thread
-import time
-import threading
+import threading, time
+from concurrent.futures import ThreadPoolExecutor
+# UI系統
+import PySimpleGUI as sg
+from ui import SimpleGUI
+# 部下
+import EventEngineer as ee
 
 class EventManager():
     def __init__(self):
+        # windowの生成
+        self.simplegui = SimpleGUI()
+        self.app_window = self.simplegui.getWindow()
+
+        # キューの生成
         self.que = deque()
         self.que.append('StandBy')
-    
+
+        # スレッド管理
+        self.executor = ThreadPoolExecutor(max_workers=3)
+
     # 命令を追加
     def order(self, order):
         self.que.append(order)
@@ -18,12 +32,51 @@ class EventManager():
         self.que.append(order)
 
     def evt_test(self):
-        print('[Event]test event.')
+        time.sleep(2)
+        self.window.logPrint('[Event]test event.')
 
     def execute_order(self):
-        thread1 = threading.Thread(target=self.evt_Test)
-        thread1.start()
+        self.future = self.executor.submit(ee.Message.test())
+
+    def len_order(self):
+        return len(self.que)
     
+    def check_order(self):
+        if self.future.done:
+            return 'done'
+        elif self.future.running:
+            return 'running'
+        else:
+            return 'Error "check_order".'
+
+    # メインのウィンドウ処理
+    def execute(self):
+        simplegui = self.simplegui
+        app_window = self.app_window
+        while True:
+            event, values = app_window.read(timeout=1000,timeout_key='-TIMEOUT-')
+            if event == sg.WIN_CLOSED or event == 'キャンセル':
+                break
+
+            # Enterキー処理
+            elif event == 'Submit':
+                input_text = app_window['-INPUT-'].get()
+                self.order(input_text)
+
+                print('You have submited %s'% input_text)
+                # ログにテキストを追加し、入力エリアをクリア
+                simplegui.logMessage(input_text)
+                app_window['-INPUT-'].update('')
+
+            elif event == 'OK':
+                print('ダウンロードURL', values[0])
+                print('ファイル保存場所', values[1])
+
+            elif event == '-TIMEOUT-':
+                time = simplegui.getTime()
+                app_window['-TIMER-'].update(time)
+
+        app_window.close()
 
 def class_test():
     print('[Sys]EventManager function.')
--- Toybox/Sangoku/main.py
+++ Toybox/Sangoku/main.py
@@ -3,36 +3,8 @@ from EventManager import EventManager
 import ui
 
 def main():
-    simpleGUI = ui.SimpleGUI()
-
-    window = simpleGUI.getWindow()
-
-    while True:
-        event, values = window.read(timeout=1000,timeout_key='-TIMEOUT-')
-        if event == sg.WIN_CLOSED or event == 'キャンセル':
-            break
-
-        # Enterキー処理
-        elif event == 'Submit':
-            input_text = window['-INPUT-'].get()
-
-            cmd_event = EventManager()
-            cmd_event.order(input_text)
-
-            print('You have submited %s'% input_text)
-            # ログにテキストを追加し、入力エリアをクリア
-            simpleGUI.logMessage(input_text)
-            window['-INPUT-'].update('')
-
-        elif event == 'OK':
-            print('ダウンロードURL', values[0])
-            print('ファイル保存場所', values[1])
-
-        elif event == '-TIMEOUT-':
-            time = simpleGUI.getTime()
-            window['-TIMER-'].update(time)
-
-    window.close()
+    app = EventManager()
+    app.execute()
 
 if __name__ == "__main__":
     main()
